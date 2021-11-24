83d1ccd5cadb219dc391f58e8b54e237bcfb816a
Bogdan9923 <sosdianbogdan@gmail.com>
2021-11-24 20:59:40 +0200

    Error fixing, and added search bar and button temporary design.

--- app/src/main/java/com/app/shoop/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/app/shoop/ui/dashboard/DashboardFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+
 import com.app.shoop.R;
 
 public class DashboardFragment extends Fragment {
